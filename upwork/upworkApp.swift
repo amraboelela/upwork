009494b42246f11eb96c994be6f0c71de1e0313a
Amr Aboelela <amraboelela@users.noreply.github.com>
2024-03-24 10:51:07 -0700

    Initial xcode project files

---
+++ upwork/upworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  upworkApp.swift
+//  upwork
+//
+//  Created by Amr Aboelela on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct upworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
